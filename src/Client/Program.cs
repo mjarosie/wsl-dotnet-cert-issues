48ededa54ce5f9562e44e084e111dfa17a6bd659
mjarosie <mjarosie@users.noreply.github.com>
2020-06-29 00:36:10 +0100

    initialise repository reproducing the issue

---
+++ src/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var apiClient = new HttpClient();
+
+            var response = await apiClient.GetAsync("https://localhost:6001/");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
